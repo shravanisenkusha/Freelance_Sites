90d905fa2156b58abc686c3c3d95c504193ff05f
shravanisenkusha <shravanip@senkusha.in>
2025-07-22 14:41:17 +0530

    Added basename to Router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,14 @@ import Contact from "./pages/Contact";
 import IndividualProduct from "./pages/IndividualProduct";
 import IndividualService from "./pages/IndividualService";
 import Testimonials from "./pages/Testimonials";
-// import Testimonial2 from "./pages/Testimonial2";
 import ComingSoon from "./pages/ComingSoon";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Freelance_Sites">
       <div className="font-sans">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/testimonial2" element={<Testimonial2 />} /> */}
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/comingsoon" element={<ComingSoon />} />
